7469fe8420cf0fe5352d892b90409a98433f7230
kieranlim-glitch <kieran.lim@coinhako.com>
2026-04-08 10:04:02 +0800

    Update wallet_report.py

--- wallet_report.py
+++ wallet_report.py
@@ -220,7 +220,7 @@ ADDR = {
 ERC20 = {
     "USDT(ERC)": {"contract": "0xdAC17F958D2ee523a2206206994597C13D831ec7", "decimals": 6},
     "USDC(ERC)": {"contract": "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", "decimals": 6},
-    "DAI": {"contract": "0x6B175474E89094C44Da98b954EedeAC495271d0F", "decimals": 18},
+    
 }
 
 
@@ -255,7 +255,6 @@ def main():
         "XLM",
         "USDT(ERC)",
         "USDC(ERC)",
-        "DAI",
         "BCH",
         "DOGE"
     ]
